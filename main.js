d5fdfad479de6116b8372d8b976d558555402b75
FleeceComrade <149838530+FleeceComrade@users.noreply.github.com>
2026-01-01 17:02:51 -0800

    Change event listener to keydown for document

--- main.js
+++ main.js
@@ -982,7 +982,7 @@ clearTilesBtn.addEventListener("click", () => {
   updateMetaUI();
 })();
 
-document.addEventListener(event => {
+document.addEventListener("keydown", event => {
   console.log("event.which: " + event.which);
   if (event.which === 192) {
     console.log("192");
